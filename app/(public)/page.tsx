df93a34d8f04c28b6902c2a061ee1efe02356db3
suphanatchanlek <suphanatchanlek@gmail.com>
2026-04-11 14:23:20 +0700

    seting font for projest

---
+++ app/(public)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to the Public Page!</h1>
+      <p className="mt-4 text-lg">
+        This page is accessible to everyone. สวัสดีครับ! นี่คือหน้าสาธารณะที่ทุกคนสามารถเข้าถึงได้ครับ!
+      </p>
+    </main> 
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Athiti, Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const athiti = Athiti({
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-athiti",
+  subsets: ["thai"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${inter.variable} ${athiti.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
