a4ab23530f0f9fe2040b97ae8caff68d31bb9eab
Napat <napat.rata@dome.tu.ac.th>
2026-04-22 17:30:16 +0700

    feat: setup leaflet

--- components/public-section/map/map-section.tsx
+++ components/public-section/map/map-section.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
+//fix bug
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
 export function MapSection() {
   return (
     <div className="w-full h-full bg-gray-300 flex items-center justify-center">
