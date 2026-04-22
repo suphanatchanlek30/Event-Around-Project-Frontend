9271a38511b0df7370cbe9f2d3327c7572ee7913
Napat <napat.rata@dome.tu.ac.th>
2026-04-22 17:15:15 +0700

    feat: add map filter

--- app/(public)/map/page.tsx
+++ app/(public)/map/page.tsx
@@ -1,11 +1,13 @@
 // app/(public)/events/page.tsx
 
 import { MapSection } from '@/components/public-section/map/map-section';
+import { MapFilter } from '@/components/public-section/map/map-filter';
 
 export default function MapPage() {
   return (
-    <main className="w-full h-full">
+    <main className="relative w-full h-full">
       <MapSection />
+      <MapFilter />
     </main>
   );
 }
---
+++ components/public-section/map/map-filter.tsx
@@ -0,0 +1,9 @@
+export function MapFilter() {
+  return (
+    <div className="absolute top-4 left-4 right-4 z-10">
+      <button className="bg-indigo-500 text-white px-4 py-2 rounded-full">
+        my location
+      </button>
+    </div>
+  );
+}
